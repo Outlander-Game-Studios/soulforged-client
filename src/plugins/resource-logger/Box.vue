3ca2e7e5c3509912ae46d497d9b56416763ce51c
simbu95 <simbu.tanksley@yahoo.com>
2024-01-21 07:32:04 -0600

    Plugin Update (#4)
    * A plugin for showing location ID and saving location image
    
    * reverting the changes I accidently made to the mock plugin
    
    * Changed method of download to not create a new dom element
    
    * Changed download url to computed object instead of method
    
    * Added plugin for copying game info to clipboard for future automation.
    
    * Added in a watch for operations, to get travel costs associated with paths when the said travel operation is selected. Also for the data already collected, now only add the data that we have found useful, to cut down on pasting/processing size
    
    * Update Box.vue
    
    Forgot about the indoors parameter
    
    * Added version of plugin that does automatic posing to the database.
    
    * Made the website string user configurable, and added an error string to be displayed if an error occurs
    
    * removed some brackets from the post request that would result in in the dictionary being sent wrong.

--- src/plugins/resource-logger/Box.vue
+++ src/plugins/resource-logger/Box.vue
@@ -21,12 +21,12 @@
     },
 	data: function() {
 		return {
-			dataDict: {'inv':{},'cre':{},'loc':{},'res':{},'path':{}}
+			dataDict: {'inv':{},'cre':{},'loc':{},'res':{},'path':{}, 'op':{}}
 		}
 	},
     subscriptions() {
       
-      //const mainEntity = GameService.getRootEntityStream();
+      const operationStream = GameService.getRootEntityStream().pluck("operation");
       const locationStream = GameService.getLocationStream();
 	  const pathStream = locationStream
               .map(({ id, paths }) => ({ id, paths }))
@@ -48,25 +48,49 @@
               )
       return {
         locationInventory: GameService.getInventoryStream(locationStream).tap((inventory) => inventory.forEach((item) => {
-			this.dataDict['inv'][item.id] = item;
-			this.dataDict['inv'][item.id]["timestamp"] = new Date().getTime();})
+			this.dataDict['inv'][item.id] = {};
+			this.dataDict['inv'][item.id]['name'] = item['name'];
+			this.dataDict['inv'][item.id]['icon'] = item['icon'];
+			this.dataDict['inv'][item.id]['durabilityStageName'] = item['durabilityStageName'];
+			this.dataDict['inv'][item.id]['amount'] = item['amount'];
+			})
 		),
         creatures: creatureStream.tap((creatures) => creatures.forEach((cre) => {
-			this.dataDict['cre'][cre.id] = cre;
-			this.dataDict['cre'][cre.id]["timestamp"] = new Date().getTime();})
+			this.dataDict['cre'][cre.id] = {};
+			this.dataDict['cre'][cre.id]['icon'] = cre['icon'];
+			this.dataDict['cre'][cre.id]['hostile'] = cre['hostile'];
+			this.dataDict['cre'][cre.id]['nonAggressive'] = cre['nonAggressive'];
+			})
 		), //Only log if hostile?
 		locationDetails: locationStream.tap((loc) => {
-			this.dataDict['loc'][loc.id] = loc;
+			this.dataDict['loc'][loc.id] = {};
+			if(loc["indoors"]) {
+				this.dataDict['loc'][loc.id]["indoors"] = loc["indoors"];
+			}
+			this.dataDict['loc'][loc.id]["spacing"] = loc["spacing"];
+			this.dataDict['loc'][loc.id]["paths"] = loc["paths"];
+			this.dataDict['loc'][loc.id]["creatures"] = loc["creatures"];
+			this.dataDict['loc'][loc.id]["resources"] = loc["resources"];
+			this.dataDict['loc'][loc.id]["inventory"] = loc["inventory"];
+			this.dataDict['loc'][loc.id]["structures"] = loc["structures"];
 			this.dataDict['loc'][loc.id]["timestamp"] = new Date().getTime();
 		}),
 		resources: resourceStream.tap((resources) => resources.forEach((resource) => {
-			this.dataDict['res'][resource.id] = resource;
-			this.dataDict['res'][resource.id]["timestamp"] = new Date().getTime();})
+			this.dataDict['res'][resource.id] = {};
+			this.dataDict['res'][resource.id]['name'] = resource['name'];
+			this.dataDict['res'][resource.id]['icon'] = resource['icon'];
+			this.dataDict['res'][resource.id]['density'] = resource['density'];
+			})
 		),
 		paths: pathStream.tap((paths) => paths.forEach((path) => {	
-			this.dataDict['path'][path.id] = path;
-			this.dataDict['path'][path.id]["timestamp"] = new Date().getTime();})
+			this.dataDict['path'][path.id] = path;})
 		),
+		operation: operationStream.tap((operation) => {
+			if (operation && operation['type'] === 'TravelOperation' && operation.context && operation.context.pathId) {
+				this.dataDict['op'][operation['context']['pathId']] = operation['context'];
+				this.dataDict['op'][operation['context']['pathId']]["timestamp"] = new Date().getTime();
+			}
+		}),
 	  };
     },
     methods: {
