774cf436cb78a63c0d0f694499b0e14b7e04a762
simbu95 <simbu.tanksley@yahoo.com>
2024-01-25 05:03:28 -0600

    Plugin bugfix (#5)
    * A plugin for showing location ID and saving location image
    
    * reverting the changes I accidently made to the mock plugin
    
    * Changed method of download to not create a new dom element
    
    * Changed download url to computed object instead of method
    
    * Added plugin for copying game info to clipboard for future automation.
    
    * Added in a watch for operations, to get travel costs associated with paths when the said travel operation is selected. Also for the data already collected, now only add the data that we have found useful, to cut down on pasting/processing size
    
    * Update Box.vue
    
    Forgot about the indoors parameter
    
    * Added version of plugin that does automatic posing to the database.
    
    * Made the website string user configurable, and added an error string to be displayed if an error occurs
    
    * removed some brackets from the post request that would result in in the dictionary being sent wrong.
    
    * fixed a problem with wrongly checking if creatures were aggressive.
    
    * Minor formatting, and added a comparison with last data sent to prevent multiple of the same data being sent frequently.
    
    * Made some changes to try to force location id to update asap since some messages where being sent for the wrong id. Also changed the duplicate data check to include node id, in case multiple nodes have the same resources, or the above problem.
    
    * moved error strings to be first in the message box to limit how often they are drawn offscreen.

--- src/plugins/resource-logger-Auto/Box.vue
+++ src/plugins/resource-logger-Auto/Box.vue
@@ -1,14 +1,15 @@
 <template>
   <HorizontalCenter>
     <Container>
+      <Spaced>{{ FetchErrors }} </Spaced>
       <Spaced>{{ LocationString }} </Spaced>
       <Spaced>{{ location.id }} </Spaced>
-      <Spaced>{{ FetchErrors }} </Spaced>
     </Container>
   </HorizontalCenter>
 </template>
 
 <script>
+
 export default {
   props: {
     location: {},
@@ -58,15 +59,13 @@ export default {
         });
       }
     }
-    const operationStream =
-      GameService.getRootEntityStream().pluck("operation");
+    const operationStream = GameService.getRootEntityStream().pluck("operation");
     const locationStream = GameService.getLocationStream();
     const pathStream = locationStream
       .map(({ id, paths }) => ({ id, paths }))
       .distinctUntilChanged(null, JSON.stringify)
       .switchMap(({ paths }) =>
-        GameService.getEntitiesStream(paths, ENTITY_VARIANTS.BASE, true)
-      );
+        GameService.getEntitiesStream(paths, ENTITY_VARIANTS.BASE, true));
     const resourceStream = locationStream
       .map(({ id, resources }) => ({ id, resources }))
       .distinctUntilChanged(null, JSON.stringify)
@@ -75,36 +74,43 @@ export default {
       .map(({ id, creatures }) => ({ id, creatures }))
       .distinctUntilChanged(null, JSON.stringify)
       .switchMap(({ creatures }) =>
-        GameService.getEntitiesStream(creatures, ENTITY_VARIANTS.BASE, true)
-      );
+        GameService.getEntitiesStream(creatures, ENTITY_VARIANTS.BASE, true));
+    var LocInv = ""
+    var CreData = ""
+    var LocData = ""
+    var OpData = ""
+    var ResData = ""
 
     return {
-      locationInventory: GameService.getInventoryStream(locationStream).tap(
-        (inventory) => {
-          //Not sure what to do about trophies still, or anything with multiple of the same icon. This could cause items to be replaced if icon is used for the update.
-          let SendData = {
-            node: this.location.id,
-            dataType: "Inventory",
-            sender: this.settings.userName,
-          };
-          let dataDict = {};
-          inventory.forEach((item) => {
-            if (dataDict[item.name]) {
-              dataDict[item.name]["amount"] =
-                item.durabilityStage == 3
-                  ? dataDict[item.name]["amount"]
-                  : dataDict[item.name]["amount"] + item.amount;
-            } else {
-              dataDict[item.name] = {};
-              dataDict[item.name]["amount"] =
-                item.durabilityStage == 3 ? 0 : item.amount;
-              dataDict[item.name]["icon"] = item.icon;
-            }
-          });
-          SendData["data"] = dataDict;
+      locationInventory: GameService.getInventoryStream(locationStream).tap((inventory) => {
+        //Not sure what to do about trophies still, or anything with multiple of the same icon. This could cause items to be replaced if icon is used for the update.
+        let SendData = {
+          node: this.location.id,
+          dataType: "Inventory",
+          sender: this.settings.userName,
+        };
+        let dataDict = {};
+        inventory.forEach((item) => {
+          if (dataDict[item.name]) {
+            dataDict[item.name]["amount"] =
+              item.durabilityStage == 3
+                ? dataDict[item.name]["amount"]
+                : dataDict[item.name]["amount"] + item.amount;
+          } else {
+            dataDict[item.name] = {};
+            dataDict[item.name]["amount"] =
+              item.durabilityStage == 3 
+                ? 0 
+                : item.amount;
+            dataDict[item.name]["icon"] = item.icon;
+          }
+        });
+        SendData["data"] = dataDict;
+        if( JSON.stringify(SendData) != LocInv) {
+          LocInv = JSON.stringify(SendData);
           SendDataToServer(SendData, this.settings, this.errorsDict);
         }
-      ),
+      }),
       creatures: creatureStream.tap((creatures) => {
         let SendData = {
           node: this.location.id,
@@ -114,7 +120,7 @@ export default {
         let dataDict = {};
         creatures.forEach((cre) => {
           if (dataDict[cre.icon]) {
-            if (dataDict[cre.nonAggressive]) {
+            if (cre.nonAggressive) {
               dataDict[cre.icon]["nonAggressive"] =
                 dataDict[cre.icon]["nonAggressive"] + 1;
             } else {
@@ -136,22 +142,28 @@ export default {
             }
           }
         });
-
         SendData["data"] = dataDict;
-        SendDataToServer(SendData, this.settings, this.errorsDict);
+        if( JSON.stringify(SendData) != CreData) {
+          CreData = JSON.stringify(SendData);
+          SendDataToServer(SendData, this.settings, this.errorsDict);
+        }
       }), //Only log if hostile?
       locationDetails: locationStream.tap((loc) => {
         let SendData = {
-          node: this.location.id,
+          node: loc.id,
           dataType: "Location",
           sender: this.settings.userName,
         };
+        this.location.id = loc.id;
         let dataDict = {};
         dataDict["spacing"] = loc["spacing"];
         dataDict["paths"] = loc["paths"];
         dataDict["structures"] = loc["structures"];
         SendData["data"] = dataDict;
-        SendDataToServer(SendData, this.settings, this.errorsDict);
+        if( JSON.stringify(SendData) != LocData) {
+          LocData = JSON.stringify(SendData);
+          SendDataToServer(SendData, this.settings, this.errorsDict);
+        }
       }),
       resources: resourceStream.tap((resources) => {
         let SendData = {
@@ -167,7 +179,10 @@ export default {
           };
         });
         SendData["data"] = dataDict;
-        SendDataToServer(SendData, this.settings, this.errorsDict);
+        if( JSON.stringify(SendData) != ResData) {
+          ResData = JSON.stringify(SendData);
+          SendDataToServer(SendData, this.settings, this.errorsDict);
+        }
       }),
       //paths: pathStream.tap((paths) => {  I am not sure we care about this data any more. I am removing it for now.
       //    SendData = { node: this.location.id, dataType: "Path", sender: this.settings.userName};
@@ -189,7 +204,10 @@ export default {
             sender: this.settings.userName,
             data: operation["context"],
           };
-          SendDataToServer(SendData, this.settings, this.errorsDict);
+          if( JSON.stringify(SendData) != OpData) {
+            OpData = JSON.stringify(SendData);
+            SendDataToServer(SendData, this.settings, this.errorsDict);
+          }
         }
       }),
     };
